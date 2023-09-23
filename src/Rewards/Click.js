bea9005b92ca4ed9db3c4fa65454fd8feb10f14e
“Abhishek <abhishek.shyam@brightchamps.com>
2023-09-23 13:53:47 +0530

    commit

--- src/Rewards/Click.js
+++ src/Rewards/Click.js
@@ -2,14 +2,14 @@ import React from "react";
 
 const Click = () => {
   return (
-    <div className="bg-gray-400 rounded-[30px] m-4 flex flex-row justify-between py-2 px-2 ">
-      <div className="w-[200px]">
+    <div className="bg-gray-400 rounded-[30px] my-4 mx-6 flex flex-row justify-between py-2 pl-2 ">
+      <div className="w-[200px] pl-1">
         <span className="text-sm">
           Invite your friends who are not on Smytten everyday
         </span>
       </div>
-      <div className="flex items-center">
-        <button className="py-1 px-3 rounded-2xl bg-sky-500">
+      <div className="flex items-center -mr-4">
+        <button className="py-1 px-3 rounded-2xl bg-sky-500 ">
           <span className="text-white">Click here</span>
         </button>
       </div>
